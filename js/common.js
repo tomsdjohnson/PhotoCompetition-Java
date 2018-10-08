eb97fb10dc4ca2a963a68fd2c603401c5df860b8
Tom Johnson <tom.johnson@softwire.com>
2018-10-08 17:24:05 +0100

    First Draft

--- js/common.js
+++ js/common.js
@@ -1,8 +1,8 @@
 'use strict';
 // JavaScript containing shared constants, for use in all pages.
 
-var backendIp = 'backend IP goes here';
-var token = 'your token goes here';
+var backendIp = '18.202.128.247';
+var token = 'd927ccc9-c9b3-4e0f-9dd6-dadfc2c7c3b6';
 
 function buildUrl(path) {
     return 'http://' + backendIp + '/images' + path + '?token=' + token;
