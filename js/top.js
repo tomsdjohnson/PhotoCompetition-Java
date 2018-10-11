3fcfec72d159c13a0cc322f859059558e1cad85b
Tom Johnson <tom.johnson@softwire.com>
2018-10-11 15:39:38 +0100

    Everything bar upload

--- js/top.js
+++ js/top.js
@@ -33,6 +33,62 @@ function loadtopImage() {
         .catch(function (err) {
             console.error('Request to /random failed: ', err);
         });
+
+    fetch(buildUrl(''))
+        .then(function (response) {
+            if (response.status !== 200) {
+                throw new Error('Request return status code !== 200: ' + response.status + ' - ')
+            }
+            return response.json();
+        })
+        .then(function (json) {
+            console.log('Request to /top succeeded: ');
+            console.log(json);
+
+            json.sort(function (a, b) {return b.score - a.score})
+            json.splice(0, 1);
+
+            for (let imageIdx in json) {
+                console.log(imageIdx);
+
+                $('#the-rest').append('\n' +
+                    '<div class="image-wrapper">\n' +
+                    '    <div class="secondary-content">\n' +
+                    '        <div class="heading">\n' +
+                    '            <span class="title" style="display: inline-block;" id="image-name">'+ json[imageIdx].name + '</span>\n' +
+                    '            <div class="info">\n' +
+                    '                <button class="info"><img class="thumbs" style="display: inline-block;" src="menu_icon.png"></button>\n' +
+                    '                <div class="dropdown-content">\n' +
+                    '                    <b>Author: </b><p id="image-author">'+ json[imageIdx].author + '</p>\n' +
+                    '                    <b>Licence: </b><p id="image-license">'+ json[imageIdx].license + '</p>\n' +
+                    '                </div>\n' +
+                    '            </div>\n' +
+                    '        </div>\n' +
+                    '\n' +
+                    '        <div class="image-container">\n' +
+                    '            <!-- src and alt attribute will be populated by index.js on page load -->\n' +
+                    '            <img id="main-image" src="'+ json[imageIdx].url + '">\n' +
+                    '        </div>\n' +
+                    '\n' +
+                    '        <div class="rank">\n' +
+                    '            <img class="top-up" id="up-vote" src="vote_up.png">\n' +
+                    '            <span class="top-text" id="image-score">'+ json[imageIdx].score + '</span>\n' +
+                    '        </div>\n' +
+                    '    </div>\n' +
+                    '</div>\n')
+            }
+
+        })
+        .catch(function (err) {
+            console.error('Request to /random failed: ', err);
+        });
+
+
+
+
+
+
+
 }
 
 $(function () {
